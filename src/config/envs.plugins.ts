54b5b43c955157ec56e96b1bef97cf68ac7445b3
Mauricio Gonzalez <mauriciogonzalezdeveloper@gmail.com>
2026-07-20 18:21:14 -0500

    Add MongoDB configuration variables to envs

--- src/config/envs.plugins.ts
+++ src/config/envs.plugins.ts
@@ -3,7 +3,11 @@ import * as env from 'env-var';
 
 export const envs = {
     PORT: env.get('PORT').required().asPortNumber(),
-    MAILER_SERVICE:env.get('MAILER_SERVICE').required().asString(),
-    MAILEAR_EMAIL: env.get('MAILEAR_EMAIL').required().asString(),
-    MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString()
+    MONGO_USER: env.get('MONGO_USER').required().asString(),
+    MONGO_PASS: env.get('MONGO_PASS').required().asString(),
+    MONGO_DB_URI: env.get('MONGO_DB_URI').required().asString(),
+    MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
+    MAILER_USER: env.get('MAILER_USER').required().asString(),
+    MAILER_PASSWORD: env.get('MAILER_PASSWORD').required().asString(),
+    MAILER_FROM_EMAIL: env.get('MAILER_FROM_EMAIL').required().asString()
 };
